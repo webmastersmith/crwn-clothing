080489e90685245fe5ec58974d413981dd8d1c35
Bryon Smith <bryon@smithauto.us>
2021-07-03 20:49:51 -0500

    update our menu-items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { HomePage } from "./pages/homepage/homepage.component.jsx";
-import Hats from "./pages/hats/hats.component";
+import Hats from "./pages/shop/hats/hats.component";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -15,7 +15,7 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map( ({title, id, size}) => <MenuItem key={id} title={title} size={size} />)}
+                {this.state.sections.map( ({id, ...otherProps }) => <MenuItem key={id} { ...otherProps } />)}
             </div>
 
         )
