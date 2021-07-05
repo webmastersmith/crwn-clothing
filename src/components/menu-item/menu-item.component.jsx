87e291d697275919a0463480e6224805b3dec654
Bryon Smith <bryon@smithauto.us>
2021-07-04 19:06:53 -0500

    fix images for shop

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,28 +1,21 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import './menu-item.styles.scss'
-import hats from '../../images/hats.png'
-import jackets from '../../images/jackets.png'
-import sneakers from '../../images/sneakers.png'
-import womens from '../../images/womens.png'
-import mens from '../../images/mens.png'
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "./menu-item.styles.scss";
 
-const pics = {
-    hats,
-    jackets,
-    sneakers,
-    womens,
-    mens
-}
-
-const MenuItem = ( {title, size, history, linkUrl, match} ) => (
-    <div className={size ? `${size} menu-item` : `menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
-        <div className="background-image" style={{backgroundImage: `url(${pics[title]})`}} ></div>
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">{'Shop Now'.toUpperCase()}</span>
-        </div>
+const MenuItem = ({ title, size, imageUrl, history, linkUrl, match }) => (
+  <div
+    className={size ? `${size} menu-item` : `menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">{"Shop Now".toUpperCase()}</span>
     </div>
-)
+  </div>
+);
 
-export default withRouter(MenuItem)
+export default withRouter(MenuItem);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import SHOP_DATA from './shop.data'
+import React from "react";
+import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            SHOP_DATA
-        }
-    }
+    this.state = {
+      SHOP_DATA,
+    };
+  }
 
-    render() {
-        return (
-            <div>
-                SHOP PAGE
-            </div>
-        )
-    }
+  render() {
+    const { SHOP_DATA: collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollections }) => (
+          <CollectionPreview key={id} {...otherCollections} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default ShopPage
+export default ShopPage;
