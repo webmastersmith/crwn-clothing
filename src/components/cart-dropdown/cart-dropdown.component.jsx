3e4126ee611f7787d09ca052d1ee11ca62d85648
Bryon Smith <bryon@smithauto.us>
2021-07-09 14:58:57 -0500

    update cart items and fix shop data id numbering

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
 
-const CartDropdown = ({ hidden }) => (
+const CartDropdown = () => (
   <div className="cart-dropdown">
     <div className="cart-items">CartDropdown</div>
     <CustomButton>Go to Checkout</CustomButton>
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,14 +4,20 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import { connect } from 'react-redux'
 import { toggleCart } from '../../redux/cart/cart.actions'
 
-const CartIcon = ({ toggleCart }) => (
+const totalItems = (cartItems) =>
+  cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
+
+const CartIcon = ({ toggleCart, cartItems }) => (
   <div className="cart-icon" onClick={toggleCart}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{totalItems(cartItems)}</span>
   </div>
 )
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+})
 const mapDispatchToProps = (dispatch) => ({
   toggleCart: () => dispatch(toggleCart()),
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
