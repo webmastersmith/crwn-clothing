20a46ef9fb3ebacf1b29837d4d907f503ce889e1
Bryon Smith <bryon@smithauto.us>
2021-07-09 10:38:07 -0500

    relocate ternary hidden to header component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import './cart-dropdown.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
-import { connect } from 'react-redux'
 
 const CartDropdown = ({ hidden }) => (
-  <div className={`cart-dropdown ${hidden ? 'hidden' : ''}`}>
+  <div className="cart-dropdown">
     <div className="cart-items">CartDropdown</div>
     <CustomButton>Go to Checkout</CustomButton>
   </div>
 )
 
-const mapStateToProps = (state) => ({
-  hidden: state.cart.hidden, //props.hidden
-})
-export default connect(mapStateToProps)(CartDropdown)
+export default CartDropdown
