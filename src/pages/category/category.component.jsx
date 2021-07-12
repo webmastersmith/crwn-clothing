5d28637aebade3a3d846ba39833af00c56e120ba
Bryon Smith <bryon@smithauto.us>
2021-07-11 22:19:09 -0500

    build collections page

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import './category.styles.scss'
+
+const CategoryPage = () => (
+  <div className="category">
+    <h2>CategoryPage</h2>
+  </div>
+)
+
+export default CategoryPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,18 @@
+import { match } from 'minimatch'
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components'
+import CategoryPage from '../category/category.component'
 
-const ShopPage = () => (
-  <div>
-    <CollectionsOverview />
-  </div>
-)
+const ShopPage = ({ match }) => {
+  console.log(match.path)
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/`} />
+    </div>
+  )
+}
 
 export default ShopPage
