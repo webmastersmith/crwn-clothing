d3847d7270d1a9181a62619aec1551ea69f8fc10
Bryon Smith <bryon@smithauto.us>
2021-07-12 16:27:09 -0500

    build collection item page, memoize shop.selection selectCollection function

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,14 @@
-import { match } from 'minimatch'
 import React from 'react'
 import { Route } from 'react-router-dom'
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.components'
-import CategoryPage from '../category/category.component'
+import CollectionPage from '../collection/collection.component'
 
 const ShopPage = ({ match }) => {
-  console.log(match.path)
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/`} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   )
 }
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,16 @@
 import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize'
 
 const selectShop = (state) => state.shop
 
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+)
+
+export const selectCollection = memoize((collectionUrlParams) =>
+  createSelector(
+    [selectCollections],
+    (collections) => (collections) => collections[collectionUrlParams]
+  )
 )
