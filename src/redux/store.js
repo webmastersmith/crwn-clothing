68c7a699d5a1056d513cf4f7bdf1d0f359342c73
Bryon Smith <bryon@smithauto.us>
2021-07-13 21:28:01 -0500

    move middlewares to development only

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger'
 import rootReducer from './root-reducer'
 
 // you can add more functions to middlewares if needed
-const middlewares = [logger]
+const middlewares = []
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
