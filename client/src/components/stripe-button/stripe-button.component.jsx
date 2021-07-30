2cd76d5abcd08adbb41fe8ef0906191373e7f2cc
Bryon Smith <bryon@smithauto.us>
2021-07-30 16:42:28 -0500

    seperated CRA app into client folder and added express server for backend of stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_JbCnolIapW323kdEwvqA6HpJ00c5NRYZCZ'
+
   const onToken = (token) => {
-    console.log(token)
-    alert('Payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token,
+      },
+    })
+      .then((response) => {
+        console.log('stripe response object', response)
+        alert('Payment Successful')
+      })
+      .catch((error) => {
+        console.log('Payment error: ', error)
+        alert(
+          'There was an issue with payment. Please use the provided test credit card.'
+        )
+      })
   }
+
   return (
     <StripeCheckout
       label="Pay Now"
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY) //imediately invoke with stripe key.
+
+//convert all incoming request 'body' tag to json. Simular to the way fetch you have to call '.json()' on it.
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+
+app.listen(port, (error) => {
+  if (error) throw error
+  console.log('Server running on http://localhost:' + port)
+})
+
+//req object comes from the client.  res is what we send to client.
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd',
+  }
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr })
+    } else {
+      res.status(200).send({ success: stripeRes })
+    }
+  })
+})
