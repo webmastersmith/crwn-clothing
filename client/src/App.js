1d4e8278e62781bd614f0ebfa7d6facc91ca5cab
Bryon Smith <bryon@smithauto.us>
2021-08-09 11:20:09 -0500

    added react-lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
 
 //page routes
 import { Switch, Route, Redirect } from 'react-router-dom'
-import HomePage from './pages/homepage/homepage.component'
-import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
-import CheckoutPage from './pages/checkout/checkout.component'
-import SignInAndSignUP from './pages/sign-in-sign-up/sign-in-and-sign-up.component'
 
 //redux
 import { connect } from 'react-redux'
@@ -14,7 +10,15 @@ import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selectors'
 import { checkUserSession } from './redux/user/user.action'
 
+import Spinner from './components/spinner/spinner.component'
 import GlobalStyle from './global.styles'
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
+const SignInAndSignUP = lazy(() =>
+  import('./pages/sign-in-sign-up/sign-in-and-sign-up.component')
+)
 
 function App({ checkUserSession, currentUser }) {
   // unsubscribeFromAuth = null
@@ -27,18 +31,20 @@ function App({ checkUserSession, currentUser }) {
     <div>
       <GlobalStyle />
       <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/checkout" component={CheckoutPage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route
-          exact
-          path="/signin"
-          render={() =>
-            currentUser ? <Redirect to="/" /> : <SignInAndSignUP />
-          }
-        />
-      </Switch>
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUP />
+            }
+          />
+        </Switch>
+      </Suspense>
     </div>
   )
 }
--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, lazy, Suspense } from 'react'
 import { Route } from 'react-router-dom'
 
 //redux
 import { connect } from 'react-redux'
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions'
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
-import CollectionsPageContainer from '../collection/collection.container'
+// import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
+// import CollectionsPageContainer from '../collection/collection.container'
+
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+)
+const CollectionsPageContainer = lazy(() =>
+  import('../collection/collection.container')
+)
 
 function ShopPage({ fetchCollectionsStart, match }) {
   useEffect(() => {
@@ -15,15 +22,17 @@ function ShopPage({ fetchCollectionsStart, match }) {
 
   return (
     <div className="shop-page">
-      <Route
-        exact
-        path={`${match.path}`}
-        component={CollectionsOverviewContainer}
-      />
-      <Route
-        path={`${match.path}/:collectionId`}
-        component={CollectionsPageContainer}
-      />
+      <Suspense>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionsPageContainer}
+        />
+      </Suspense>
     </div>
   )
 }
