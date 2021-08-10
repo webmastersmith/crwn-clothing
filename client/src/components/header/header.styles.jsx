dc2b5271133043111ab0dce8df8927bd4e98996e
Bryon Smith <bryon@smithauto.us>
2021-08-09 22:54:50 -0500

    added error boundary support

--- client/src/components/header/header.styles.jsx
+++ client/src/components/header/header.styles.jsx
@@ -20,7 +20,7 @@ export const LogoContainer = styled(Link)`
   position: relative;
   height: 100%;
   width: 70px;
-  padding: 25px;
+  /* padding: 25px; */
 
   @media screen and (max-width: 800px) {
     width: 50px;
