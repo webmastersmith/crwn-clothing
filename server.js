a8e09b60a7745cb0e06b962d0680184fcba94dc6
Bryon Smith <bryon@smithauto.us>
2021-08-11 19:18:29 -0500

    update code to enable PWA

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const compression = require('compression')
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
